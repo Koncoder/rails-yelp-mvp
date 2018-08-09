9c18b6bc971576865800025a03c69279643fcd17
Konstantin Baumer <baumer.konstantin@gmail.com>
2018-08-09 11:02:47 -0400

    Still need to add a reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "restaurants#index"
+
+  get "restaurants", to: "restaurants#index", as: "restaurants"
+
+  post "restaurants", to: "restaurants#create"
+
+  get "restaurants/new", to: "restaurants#new"
+
+  get "restaurants/:id", to: "restaurants#show", as: "restaurant"
+
+
+
 end
